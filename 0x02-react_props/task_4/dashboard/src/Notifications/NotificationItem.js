f4bbf9eeec785de18ad28a9f3d168212d2913983
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-05-17 10:53:24 +0300

    update 5 files

--- 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { getLatestNotification }  from '../utils/utils'
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
 
-export function AllNotifyItems(){
+export function AllNotifyItems() {
     return (
         <React.Fragment>
-            <NotificationItem type='default' value='New course available' />
-            <NotificationItem type='urgent' value='New resume available' />
-            <NotificationItem type='urgent' html={{__html:getLatestNotification()}} />
+            <NotificationItem type="default" value="New course available" />
+            <NotificationItem type="urgent" value="New resume available" />
+            <NotificationItem
+                type="urgent"
+                html={{ __html: getLatestNotification() }}
+            />
         </React.Fragment>
-    )
+    );
 }
 
-export function NotificationItem({type, html, value}){
-        if (html === undefined)
-            return (
-                <li data-priority={type}>
-                    {value}
-                </li>
-            );
-        return (
-                <li data-priority={type} dangerouslySetInnerHTML={html}></li>
-            );
-    };
+export default function NotificationItem({ type, html, value }) {
+    if (html === undefined) return <li data-priority={type}>{value}</li>;
+    return <li data-priority={type} dangerouslySetInnerHTML={html}></li>;
+}
 
-export default NotificationItem;
+NotificationItem.propTypes = {
+    html: PropTypes.shape({
+        __html: PropTypes.string.isRequired,
+    }),
+    type: PropTypes.string,
+    value: PropTypes.string.isRequired,
+};
+
+NotificationItem.defaultProps = {
+    type: 'default',
+};
