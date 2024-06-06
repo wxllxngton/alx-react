7b770b6424a3bd1aaabb5dde55cf98c4b5da5f60
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-06-06 12:42:33 +0300

    update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,14 +1,18 @@
 import { Map } from './node_modules/immutable/dist/immutable.js';
 
 export const map = Map({
-    1: 'Liam',
-    2: 'Noah',
-    3: 'Elijah',
-    4: 'Oliver',
-    5: 'Jacob',
-    6: 'Lucas',
+    "1": 'Liam',
+    "2": 'Noah',
+    "3": 'Elijah',
+    "4": 'Oliver',
+    "5": 'Jacob',
+    "6": 'Lucas'
 });
 
-export const map2 = map.withMutations((mapItem) =>
-    mapItem.set(2, 'Benjamin').set(4, 'Oliver')
-);
+export const map2 = map.withMutations((mapItem) => mapItem.set(
+    2,
+    'Benjamin'
+).set(
+    4,
+    'Oliver'
+));
