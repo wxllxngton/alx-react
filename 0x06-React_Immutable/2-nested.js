0e7a9d0b3239d0139f0211a2ab2971fb04ab254c
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-06-06 11:29:37 +0300

    feat: create 2-nested.js

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,15 @@
+import { Map } from './node_modules/immutable/dist/immutable.js';
+
+/**
+ * Returns the value of the object at the defined path.
+ *
+ * @param {Object} object - The plain object.
+ * @param {Array} array - Array containing the path to a key in the object.
+ * @returns {String|Map|Number|undefined} The value at the defined path,
+ * or undefined if the path does not exist.
+ */
+export default function accessImmutableObject(object, array) {
+    const mapObj = Map(object);
+
+    return mapObj.getIn(array);
+}
