2218dd002f160bfe2eb563b07c43a6c5633faedb
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-06-06 12:36:50 +0300

    feat: create 4-mutations.js

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,12 @@
+import { Map } from './node_modules/immutable/dist/immutable.js';
+
+export const map = Map({
+    1: 'Liam',
+    2: 'Noah',
+    3: 'Elijah',
+    4: 'Oliver',
+    5: 'Jacob',
+    6: 'Lucas',
+});
+
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
