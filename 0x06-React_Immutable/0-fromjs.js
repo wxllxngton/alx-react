f61067ffe4e966fffb5127e6aeff3bc20f41a602
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-06-06 09:29:02 +0300

    update 6 files

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -6,15 +6,6 @@ import { fromJS } from './node_modules/immutable/dist/immutable.js';
  * @param {Object} object - The object to be converted.
  * @returns {Immutable.Map} An immutable Map representation of the input object.
  */
-export default function getImmutableObject(object) {
-    return fromJS(object);
+export default function getImmutableObject (object) {
+  return fromJS(object);
 }
-
-console.log(
-    getImmutableObject({
-        fear: true,
-        smell: -1033575916.9145899,
-        wall: false,
-        thing: -914767132,
-    })
-);
